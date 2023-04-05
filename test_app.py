44608bcfabfec4ed37d80b413882876b6ec51a22
Richard Aydan Fiuza Ost <122907674+aydanfiuza@users.noreply.github.com>
2023-04-05 20:11:05 -0300

    coverage aumentado em 93%

--- test_app.py
+++ test_app.py
@@ -37,3 +37,9 @@ def test_appMessage():
 
     assert True == checkLenghtMsg('Eu gosto de pizza.')
     assert False == checkLenghtMsg('Nulla facilisi. Praesent volutpat augue sit amet diam varius, in ultricies justo tristique. Fusce pharetra nisi at est sagittis, nec elementum ipsum maximus.')
+
+    assert True == checkLenghtMsg('A vida é curta e bela, aproveite cada momento.')
+    assert False == checkLenghtMsg('O sucesso é a soma de pequenos esforços repetidos dia após dia. Não pare nunca de persistir e tentar.')
+
+    assert True == checkLenghtMsg('A vida é agora, aproveite o momento.')
+    assert False == checkLenghtMsg('Acredite em si mesmo e em seu potencial. Seja paciente, persistente e trabalhe duro. Grandes coisas levam tempo e esforço, mas a recompensa será incrível.')
