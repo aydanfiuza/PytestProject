f07c34d934f570165ad1d5b4649fd272e02f6942
Richard Aydan Fiuza Ost <122907674+aydanfiuza@users.noreply.github.com>
2023-04-05 22:57:39 -0300

    versão finalizada

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ def checkUser():
     while True:
         login = askUser()
         if checkUpper(login) and checkSpace and checkSpecial(login) and checkLenght(login):
-            break
+            return login
         else:
             print("O usuário não satisfaz as requisições de login.\nVocê deve conter a primeira letra maiúscula, nenhum caracter especial e menos de 30 caracteres ao todo.")
     
 def checkPassword():
     while True:
-        psw = askPassword()
-        if checkLenghtPsw(psw) and checkSpecialPsw(psw) and not checkNumberPsw(psw) and not checkUpperPsw(psw) and not checkLowerPsw(psw):
-            break
+        password = askPassword()
+        if checkLenghtPsw(password) and checkSpecialPsw(password) and checkNumberPsw(password) and checkUpperPsw(password) and checkLowerPsw(password):
+            return password
         else:
             print("As requisições de senha não foram satisfeitas.\nVocê deve conter ao menos uma letra maiúscula, uma minúscula, um caracter especial, um número e mais de 10 caracteres.")
 
@@ -23,21 +23,24 @@ def checkMessage():
     while True:
         message = askMessage()
         if checkLenghtMsg(message):
-            break
+            return message
         else:
             print("A requisição de mensagem não foi satisfeita.\nVocê precisa escreve-la com menos de 70 caracteres.")
 
 
 login = checkUser()
 print("Usuário válido.")
-psw = checkPassword()
+password = checkPassword()
 print("Senha válida.")
 message = checkMessage()
 print("Sua mensagem foi armazenada e criptografada com sucesso.")
 
 initializeWrite()
-encryptedText = encryptMessage(login, psw, message)
+encryptedText = encryptMessage(login, password, message)
 saveNewLine(encryptedText)
+
 initializeRead()
 line1 = readNextLine()
-print(line1)
+print('--------------------------')
+print(f'Mensagem criptografada:\n{line1}')
+print('--------------------------')
--- appPasswordFunctions.py
+++ appPasswordFunctions.py
@@ -1,33 +1,33 @@
 def askPassword():
-    psw = input("Informe sua senha: ")
-    return psw
+    password = input("Informe sua senha: ")
+    return password
 
-def checkLenghtPsw(psw):
-    if len(psw) < 10:
+def checkLenghtPsw(password):
+    if len(password) < 10:
         return False
     else:
         return True
 
-def checkSpecialPsw(psw):
-    if not any(not c.isalnum() for c in psw):
+def checkSpecialPsw(password):
+    if not any(not c.isalnum() for c in password):
         return False
     else: 
         return True
     
-def checkNumberPsw(psw):
-    if any(i.isdigit() for i in psw):
+def checkNumberPsw(password):
+    if any(i.isdigit() for i in password):
         return True
     else:
         return False
 
-def checkUpperPsw(psw):
-    if any(char.isupper() for char in psw):
+def checkUpperPsw(password):
+    if any(char.isupper() for char in password):
         return True
     else:
         return False
 
-def checkLowerPsw(psw):
-    if any(char.islower() for char in psw):
+def checkLowerPsw(password):
+    if any(char.islower() for char in password):
         return True
     else:
         return False
